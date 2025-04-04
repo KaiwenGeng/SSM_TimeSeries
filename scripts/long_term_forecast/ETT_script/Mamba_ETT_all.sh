d5c0c81c7091bca7dc2aa9495c43bf37f6984c14
KaiwenGeng <168481232+KaiwenGeng@users.noreply.github.com>
2025-04-04 01:25:13 -0400

    Delete scripts directory

--- scripts/long_term_forecast/ETT_script/Mamba_ETT_all.sh
+++
@@ -1,4 +0,0 @@
-./scripts/long_term_forecast/ETT_script/Mamba_ETTh1.sh
-./scripts/long_term_forecast/ETT_script/Mamba_ETTh2.sh
-./scripts/long_term_forecast/ETT_script/Mamba_ETTm1.sh
-./scripts/long_term_forecast/ETT_script/Mamba_ETTm2.sh
